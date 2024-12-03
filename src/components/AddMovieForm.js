29a62dec021b71a48eba90a84753bddfb02127f2
Johnson <23009886@myrp.edu.sg>
2024-12-03 15:11:23 +0800

    initial commit

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -5,7 +5,19 @@ export default function AddMovieForm({ onAddMovie }) {
 
   const handleSubmit = (e) => {
     // TODO: write code to handle form submission
+    e.preventDefault();
+
+    if (!title.trim()) {
+      return;
+    }
+    onAddMovie(title.trim());
+    setTitle("");
   };
 
-  return; // TODO: add a form to add a new movie
+  return(// TODO: add a form to add a new movie 
+    <form onSubmit={handleSubmit}>
+      <input placeholder="Movie Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+      <button>Add movie</button>
+    </form>
+  );
 }
